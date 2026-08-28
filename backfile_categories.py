bcc9893e8208f72a00db1cea9e92d005edc72230
Harsh Yadav <harsh.yadav@partner.htmedialabs.com>
2026-08-28 23:38:39 +0530

    Added pagination, categoriesc

--- backfile_categories.py
+++ backfile_categories.py
@@ -1,7 +1,9 @@
-from tier_store import apply_tier_schema, backfill_book_categories
+from tier_store import apply_tier_schema, apply_book_categories, backfill_book_categories
 
 print("Applying tier schema...")
 apply_tier_schema()
+print("Applying book categories schema...")
+apply_book_categories()
 
 print("Backfilling book categories...")
 count = backfill_book_categories()
